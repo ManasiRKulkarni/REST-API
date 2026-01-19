bec05d0d1164122db8d70df375f9abbc52e8317f
ManasiRKulkarni <92688285+ManasiRKulkarni@users.noreply.github.com>
2026-01-19 18:47:51 +0530

    Add files via upload

---
+++ CloudVendorAPIService.java
@@ -0,0 +1,34 @@
+package com.example.demo.controller;
+
+import com.example.demo.model.CloudVendor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/cloudvendor")
+public class CloudVendorAPIService {
+
+    CloudVendor cloudVendor;
+    @GetMapping("{vendorId}")
+    public CloudVendor getCloudVendorId(String vendorId){
+        //return new CloudVendor("C1","Vendor1","Address1","XXXX");
+        return cloudVendor;
+    }
+
+    @PostMapping
+    public String createCloudVendorDetails(@RequestBody CloudVendor cloudVendor){
+        this.cloudVendor = cloudVendor;
+        return "Cloud Vendor Created Successfully";
+    }
+
+    @PutMapping
+    public String updateCloudVendorDetails(@RequestBody CloudVendor cloudVendor){
+        this.cloudVendor = cloudVendor;
+        return "Cloud Vendor Updated Successfully";
+    }
+
+    @DeleteMapping("{vendorId}")
+    public String deleteCloudVendorDetails(String vendorId){
+        this.cloudVendor = null;
+        return "Cloud Vendor Deleted Successfully";
+    }
+}
